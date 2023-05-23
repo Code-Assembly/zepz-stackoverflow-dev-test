e49791ad2b86f1a996d5339bdb9ae4c2a3b9e484
Aadil Cachalia <aadil@codeassembly.co.za>
2023-05-23 16:34:29 +0200

    Feat: Clean-up Application scaffold and remove unused files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { Container, CssBaseline, GlobalStyles } from '@mui/material';
+import { grey } from '@mui/material/colors';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+const App: FC = () => {
+	return (
+		<>
+			<CssBaseline />
+			<GlobalStyles
+				styles={{
+					body: { backgroundColor: grey[100] },
+				}}
+			/>
+
+			{/* In future this would become an app shell */}
+			<Container component="main" sx={{ maxWidth: 'md' }}>
+				{/* This would likely contain routing to additional pages */}
+			</Container>
+		</>
+	);
+};
 
 export default App;
